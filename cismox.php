13f9b999d9102f4b57ffc0469c190c640cc49f36
gajjardarshithasmukhbhai <darshit.gajjar1998@gmail.com>
2018-07-13 11:27:07 +0700

    cc,c
    ,x,x

---
+++ cismox.php
@@ -0,0 +1,278 @@
+<html>
+<head>
+    <title></title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="css/animate.css">
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="cismox.css">
+    <link rel="stylesheet" type="text/css" href="cismox1.css">
+    
+    <meta id="meta" name="viewport"
+          content="width=device-width; initial-scale=1.0" />
+    <style type="text/css">
+        #load_screen{
+            background-color: white;
+            opacity: 1;
+            position: fixed;
+            z-index: 999999;
+            top: 0px;
+            width: 100%;
+            height: 1600px;
+        }
+        #load_screen>#loading{
+            width: 90px;
+            height: 90px;
+            position: absolute;
+            top: 250px;
+            left: 620px;
+            right: 350px;
+            -webkit-animation: rotation 2s infinite linear;
+}
+@-webkit-keyframes rotation {
+        from {
+                -webkit-transform: rotate(0deg);
+        }
+        to {
+                -webkit-transform: rotate(359deg);
+        }
+}
+.gtr{
+    position: absolute;
+            top: 360px;
+            left: 609px;
+            right: 350px;
+            color: #1461ED;
+}
+.icon2{
+    width: 80px;
+    height: 80px;
+}
+.iconw{
+    width: 120px;
+    height: 80px;
+    color: white;
+}
+.span{
+    color: #FFFFFF;
+    font-size: 20px;
+}
+.lmdk{
+         background-image: url("fram.png");
+        background-size: cover;
+        height: 680px;
+  }
+    </style> 
+    <script type="text/javascript">
+        window.addEventListener("load",function () {
+            var load_screen=document.getElementById("load_screen");
+        document.body.removeChild(load_screen);
+        });
+        </script>
+    </head>
+<body>
+    <div id="load_screen"><img src="Gear.png" align="middle" id="loading"/><br><h1 class="gtr wow fadeIn">cismox</h1></div>
+<nav class="navbar navbar-default navbar-fixed-top kk" role="navigation lp">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-container">
+                <span class="icon-bar"></span>
+                <span class="icon-bar "></span>
+                <span class="icon-bar"></span>
+            </button>
+            <h1 class="font"><img src="jlk.png" class="ui">cismox</h1>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar-container">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="btn blk"><a href="kdkd.html"><p class="pll"><span class="glyphicon glyphicon-log-in"></span> Login</p></a></li>
+                <li class="btn"><a href=""><p class="oop"><span class="glyphicon glyphicon-pencil"></span>Signup</p></a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<script type="text/javascript">
+    $(document).ready(function(){
+ $(document).bind("contextmenu",function(e){
+   return false;
+ });
+});
+</script>
+
+
+
+<div class="jumbotron lmdk">
+    <br/>
+    <br/>
+    <br/>
+    <div class="container">
+        <div class="text dser">why cismox Not other?</div>
+    </div>
+    <br>
+    <div class="head">
+        
+    </div>
+    <br/>
+    <br/>
+    
+        <div class="col-md-12">
+            <div class="col-md-1"></div>
+            <div class="col-md-9">
+                <img src="icon2.png" class="icon2 wow bounceInUp"><span class="span">Fastest growing Tech startup in INDIA</span><br><br><br>
+                <img src="icon2.png" class="icon2 wow bounceInUp"><span class="span">Take courses with you online and offline</span><br><br><br>
+                <img src="icon2.png" class="icon2 wow bounceInUp"><span class="span">Any problem contact our Customer care 1200-120-320</span>
+            </div>
+            <div class="col-md-2"></div>
+                
+        </div>
+    
+</div>
+<br><br><br>
+<br><br><br>
+
+<div class="row">
+<div class="container">
+    <div class="col-md-12">
+            <div class="col-md-3">
+                <img src="web.png" width="220px" height="220px" class="dox wow rollIn ripple">
+                <p class="website">Our Company Making the Website Like Static and Dynamic both will be Making</p>
+                <button type="button" class="btn btn-lg de ripple"><span class="glyphicon glyphicon-ok tree"></span><b>click</b></button>
+            </div>
+            <div class="col-md-3">
+                <img src="android.png" width="220px" height="220px" class="dox wow rollIn">
+                <p class="website">Our Company Making ANDROID App shortly show We take project very shoon</p>
+                <button type="button" class="btn btn-lg de ripple"><span class="glyphicon glyphicon-ok tree"></span><b>click</b></button>
+            </div>
+        <div class="col-md-3">
+            <img src="designing.png" width="220px" height="220px" class="dox wow rollIn">
+            <p class="website">Our Company Making the Website Design for Client Requirement</p>
+                <button type="button" class="btn btn-lg de ripple"><span class="glyphicon glyphicon-ok tree"></span><b>click</b></button>
+        </div>
+        <div class="col-md-3">
+            <img src="KDLKLD.png" width="220px" height="220px" class="dox wow rollIn">                        <p class="website">Our Company staring new working domain it's IOT.we start new business coming soon</p>
+                <button type="button" class="btn btn-lg de ripple"><span class="glyphicon glyphicon-ok tree"></span><b>click</b></button>
+        </div>
+    </div>
+    </div>
+    <i class="fa fa-arrow-right"></i>
+
+    <br>
+    <br>
+    <div class="col-md-12">
+        <div class="col-md-3"></div>
+        <div class="col-md-3">
+            <img src="dll.png" width="220px" height="220px" class="dox wow rollIn">            
+                <p class="website">Our Company solve the web Bug and penetration testing your website any firm interested contact our company </p>
+                <button type="button" class="btn btn-lg de ripple"><span class="glyphicon glyphicon-ok tree"></span><b>click</b></button>
+        </div>
+        <div class="col-md-3">
+            <img src="SEO.png" width="220px" height="220px" class="dox wow rollIn">            
+                <p class="website">Our Company give SEO services.new client give free advice then after 1 year take charge</p>
+                <button type="button" class="btn btn-lg de ripple"><span class="glyphicon glyphicon-ok tree"></span><b>click</b></button>
+        </div>
+        <div class="col-md-3"></div>
+    </div>
+</div>
+    <br>
+    <br>
+    <div class="lop">
+    <img src="technology.png" width="220px" height="220px" class="dox wow rollIn">            
+        <p class="website">Our Company use the react js,<br>node js,oop php,bootstrap 3,css 3,<br>animation,MY-SQL,Java script,Express js<br>Enceypted alogirithem use for Encryption</p>
+    </div>
+</div>
+<br><br>
+<br><br>
+<br><br>
+<br><br>
+<footer class="page-footer ipman bn ripple">
+    <br>
+        <div class="container">
+            <div class="col-lg-12">
+                <div class="col-lg-4">
+                    <h3 class="office">Office</h3>
+                        <div class="col-lg-12 col-sm-4 col-md-4">
+                            <p class="text ret "><b>>></b>E-204 vinod Tower,shrinath recendency<br>pajarpol,kadi</p>
+                            <p class="text ret"><b class="zoomw">>></b>phone number:7984552350</p>
+                        </div>
+                </div>
+                <div class="col-lg-4"></div>
+                <div class="col-lg-4">
+                    <h3 class="office">Book your project</h3>
+                    <p class="text ret"><b>>></b>booking your project you give the 10% advance d given<br>then after your project start</p>
+                        <button type="button" class="project smn1 ripple"><b>Project</b></button>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-12">
+            <div class="col-md-2">
+            </div>
+            <div class="col-md-2">
+                <p class="lop"><img src="github.png" width="38px" height="38px" class="zoom"><b>      Github</b></p>
+            </div>
+            <div class="col-md-2">
+                <p class="lop"><img src="youtubeheb"  width="38px" height="38px" class="zoom"><b>  youtube</b></p>
+            </div>
+            <div class="col-md-2">
+                <p class="lop"><img src="gmaiil.png" width="38px" height="38px" class="zoom"><b>  Email-id</b></p>
+            </div>
+            <div class="col-md-2">
+                <p class="lop"><img src="link.png" width="38px" height="38px" class="zoom"><b>  LinkedIn</b></p>
+            </div>
+            <div class="col-md-2">
+            </div>
+        </div>
+        <div class="container">
+            <div class="col-lg-12">
+                <div class="col-lg-4">
+                    <h3 class="office"> Support & feedback</h3>
+                        <div class="col-lg-12 col-sm-4 col-md-4">
+                            <p class="text ret "><b></b>you can send feedback and get support via email and otherwise contact our customer care</p>
+                        </div>
+                </div>
+                <div class="col-lg-4">
+                </div>
+                <div class="col-sm-4">
+                    <h3 class="office">E-mail</h3>
+                    <div class="col-sm-12 col-lg-12 col-md-4">
+                            <p class="text jk "><b></b>cismox.code@gmail.com</p>
+                        </div>    
+                </div>
+        </div>
+    </div>
+        <div class="col-md-12">
+                <h3 class="lk">copyright 2018|all right reserved</h3>
+        </div>
+    </div>
+</footer>
+<!-- Return to Top -->
+<a href="javascript:" id="return-to-top"><i class="icon-chevron-up wow flip "></i></a>
+
+
+<!-- ICON NEEDS FONT AWESOME FOR CHEVRON UP ICON -->
+<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+
+  
+<!-- Test the scroll -->
+<script type="text/javascript">
+    $(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
+</script>
+<script src="js/wow.min.js"></script>
+<script>
+    new WOW().init();
+</script>
+<script src="js/wow.min.js"></script>
+<script>
+    new WOW().init();
+</script>
+</body>
+</html>
